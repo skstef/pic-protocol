f955cdbf86217bdc3b8d34a10d92762f3bc8cedc
skstef <sstefstefv@gmail.com>
2026-01-09 15:21:03 +0200

    feature: colors interim

--- src/components/TrioSection.tsx
+++ src/components/TrioSection.tsx
@@ -21,27 +21,29 @@ const ITEMS = [
 
 export const TrioSection = () => {
   return (
-    <div id="tw-scope" className="grid grid-cols-3 py-40 px-24">
-      {ITEMS.map((item) => (
-        <div
-          key={item.title}
-          className="flex flex-col items-center text-center px-6.5"
-        >
-          <div className="bg-[#00543D]/5 rounded-full mb-14 aspect-square flex items-center justify-center">
-            <img
-              src={item.imageURL}
-              alt={item.title}
-              className="w-70 h-70 mb-6"
-            />
+    <div id="tw-scope" className="py-40 px-24 dark:bg-[#1F1F21]">
+      <div className="max-w-360 mx-auto grid grid-cols-3">
+        {ITEMS.map((item) => (
+          <div
+            key={item.title}
+            className="flex flex-col items-center text-center px-6.5"
+          >
+            <div className="bg-[#00543D]/5 dark:bg-white/5 rounded-full mb-14 aspect-square flex items-center justify-center">
+              <img
+                src={item.imageURL}
+                alt={item.title}
+                className="w-70 h-70 mb-6"
+              />
+            </div>
+            <h3 className="text-black dark:text-white font-[Clash_Grotesk]! tracking-[-0.32px] text-2xl font-medium leading-6 mb-4">
+              {item.title}
+            </h3>
+            <p className="text-[#9C9C9C] dark:text-white/50 tracking-[-0.32px] text-base font-normal leading-6">
+              {item.description}
+            </p>
           </div>
-          <h3 className="text-black  font-[Clash_Grotesk]! tracking-[-0.32px] text-2xl font-medium leading-6 mb-4">
-            {item.title}
-          </h3>
-          <p className="text-[#9C9C9C] tracking-[-0.32px] text-base font-normal leading-6">
-            {item.description}
-          </p>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
