f26c7cba67c7e2dd008c5513ca104e31b0a0121c
Nicola Gallo <nicola.gallo@nitroagility.com>
2026-02-07 17:11:54 +0100

    Updated pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "PIC",
-  tagline: "Provenance Identity Continuity Protocol",
+  tagline: "Provenance Identity Continuity",
   favicon: "img/favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
