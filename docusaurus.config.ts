56c9da28ab67c88a77fb7fc75270115d2a5e1441
skstef <sstefstefv@gmail.com>
2026-02-09 00:54:58 +0200

    refactor: added www to og image url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://pic-protocol.org",
+  url: "https://www.pic-protocol.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -113,3 +113,7 @@ const config: Config = {
 };
 
 export default config;
+
+
+
+https://pic-protocol.org/img/social-card.jpg
